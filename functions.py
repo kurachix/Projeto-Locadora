47c90efe2e8a3ccf97b96f80ab80ae447be6f77c
kurachix <matheuskurachi6@gmail.com>
2025-09-09 12:46:36 -0300

    Criação do menu, e alocação da locadora do carlão

--- functions.py
+++ functions.py
@@ -5,4 +5,24 @@ from classes import *
 import os
 import time
 
+LocadoraDoCarlao = Locadora("Locadora_do_Carlão", "Jundiai-SP", "11 961863232")
+
+def cadastro_itens():
+
+    while True:
+        os.system("cls")
+
+        print(30 * "-")
+        print("CADASTRO DE ITENS".center(30))
+        print(30 * "-")
+
+        print("\nO que deseja cadastrar hoje?")
+        print("\n1 -> Jogo")
+        print("2 -> Filme")
+        print("0 -> Voltar")
+
+        cadastro_escolha = int(input("\n Qual a sua escolha? ->"))
+
+        
+
 
